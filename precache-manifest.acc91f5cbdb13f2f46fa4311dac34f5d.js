72d71078e917488733ab5aa47d722d80cd6c1bf4
rakshithjk <rakshithjayakumar@gmail.com>
2022-07-10 16:00:17 +0530

    Updates

---
+++ precache-manifest.acc91f5cbdb13f2f46fa4311dac34f5d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caee7a2017329149f860ceb8c08e57a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad811e5a185da6ee22c5",
+    "url": "/static/css/main.051af466.chunk.css"
+  },
+  {
+    "revision": "7b7b7aedab4e081474eb",
+    "url": "/static/js/2.a285f707.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.a285f707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad811e5a185da6ee22c5",
+    "url": "/static/js/main.162e3f1a.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "3e274bbdd34cc23e400ebbb91f5502e4",
+    "url": "/static/media/features-split-image-01.3e274bbd.png"
+  },
+  {
+    "revision": "5c8ee34b25521b105848b9a0b1864719",
+    "url": "/static/media/features-split-image-02.5c8ee34b.png"
+  },
+  {
+    "revision": "9552a20370275f7aa20db9e5085d1f96",
+    "url": "/static/media/features-split-image-03.9552a203.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "1dcdf7253c282ba5104e6101b7b57962",
+    "url": "/static/media/logo.1dcdf725.svg"
+  }
+]);
